8e918f3317c02e2976fc243146ffc9668c4114c7
Javier241095 <137368130+Javier241095@users.noreply.github.com>
2024-06-16 02:02:48 -0300

    menu hamburguesa
    menu al 95%

--- js/menuActivo.js
+++ js/menuActivo.js
@@ -1,13 +1,17 @@
 'use stryc'
 let d = document;
 
+/**
+ Desde la linea de codigo 8 hasta la 24 se encarga de ACTIVAR(mostrar) y DESACTIVAR(ocultar) el menu hamburguesa.
+**/
+
 let MenuButton = d.querySelector('nav > button');
 let navUl = d.querySelector('header div nav div');
 
 MenuButton.addEventListener('click', ()=>{
     //Esta clase se aplica para animar el icono del menu cambiando al icono de cerra (X) y viseversa.
     MenuButton.classList.toggle('activoX')
-    //Esta clase se aplica para mostra y ocultar el menu de navegacion.
+    //Esta clase se aplica para mostra y ocultar el menu HAMBURGUESA.
     navUl.classList.toggle('menuVisible')
 })
 
@@ -19,24 +23,65 @@ navUl.addEventListener('click', (e)=>{
     }
 })
 
-//La class "activo" esta en la linea 125.
-//La class "menuVisible" esta en la linea 93 hasta la 103.
+//La class "activo" esta en la linea 125 en CSS.
+//La class "menuVisible" esta en la linea 93 hasta la 103 en CSS.
 
+/**
+ Desde la linea de codigo 33 hasta la 24 se encarga
+**/
 
-let menu = d.querySelectorAll('header > div nav > div')[0]
+let SubMenus = d.querySelectorAll('header > div nav ul .subMenus')
+let spanFlecha = d.querySelectorAll('header > div nav ul li span')
 
-let altoMenu = `${menu.clientHeight - 100}px`;
-menu.style.setProperty("--altoMenu", altoMenu);
+SubMenuActivo(spanFlecha[0], 0, 1);
 
-let button =  d.querySelector('header > div nav > div button')
+SubMenuActivo(spanFlecha[1], 1, 0);
 
-let subMenu = d.querySelectorAll('header > div nav ul .subMenu')
-let liUl = d.querySelectorAll('header > div nav ul li span')
-
-for (let i = 0; i < subMenu.length; i++) {
-    subMenu[i].addEventListener('click', (e)=>{
-        subMenu[i].classList.toggle('marginTop')
-        subMenu[i].classList.toggle('subMenu')
-        liUl[i].classList.toggle('arrow')
-    })
+function SubMenuActivo( spanFlecha = [''], flechaActiva = 0, flechaDesactiva = 1){
+  for (let i = 0; i < SubMenus.length; i++) {
+      SubMenus[i].addEventListener('click', (e)=>{
+        //Esta variable se utiliza para buscar el hermano siguiente del SPAN que seria el UL donde se encuentra la lista del SUBMENU
+        let listaSubMenu = spanFlecha.nextElementSibling;
+  
+        if(i == flechaActiva){
+          //esta clase se aplica para rotar la flecha hacia abajo para indicar que esta desplegado el SUBMENU
+          spanFlecha.classList.toggle('flecha_abajo')
+          //Esta variable guarda el alto obtenido del "scrollHeight" espacio necesario para mostrar el listado del SUBMENU
+          let altoSubMenu = `${listaSubMenu.scrollHeight}px`;
+          //Esta variable se encarga de aplicar el valor del "height" para mostra el SUBMENU
+          listaSubMenu.style.height = listaSubMenu.style.height == altoSubMenu ?  '0px': altoSubMenu;
+          listaSubMenu.classList.toggle('altoSubMenu')
+        } else if(i == flechaDesactiva ){
+          listaSubMenu.style.height = '0px'
+          listaSubMenu.classList.remove('altoSubMenu')
+          spanFlecha.classList.remove('flecha_abajo')
+        }
+      })
+      
+  }
 }
+
+/*
+for (let i = 0; i < SubMenus.length; i++) {
+    SubMenus[i].addEventListener('click', (e)=>{
+        //esta clase se aplica para rotar la flecha hacia abajo para indicar que esta desplegado el sub menu
+        let listaSubMenu = spanFlecha[1].nextElementSibling;
+
+        console.log(i)
+
+      if(i == 1){
+        spanFlecha[0].classList.toggle('flecha_abajo')
+
+        let altoSubMenu = `${listaSubMenu.scrollHeight}px`;
+    
+        listaSubMenu.style.height = listaSubMenu.style.height == altoSubMenu ?  '0px': altoSubMenu;
+        listaSubMenu.classList.toggle('altoSubMenu')
+      } else if(i == 0){
+        listaSubMenu.style.height = '0px'
+        listaSubMenu.classList.remove('altoSubMenu')
+        spanFlecha[1].classList.remove('flecha_abajo')
+      }
+   
+    })
+    
+}*/
