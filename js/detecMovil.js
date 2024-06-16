ff086ef905c19c379cb3ca2993fc9915264ec31e
Javier241095 <137368130+Javier241095@users.noreply.github.com>
2024-06-16 16:33:26 -0300

    banner modificaciones

--- js/detecMovil.js
+++ js/detecMovil.js
@@ -1,8 +1,9 @@
 let navegador = navigator.userAgent;
 let header = d.querySelector('header');
 if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-    header.classList.remove('movil');
-} else {
     header.classList.add('movil');
+   
+} else {
+    header.classList.remove('movil');
 }
 //Este Js es para dectectar si se esta usando el nevegador en el movil o la pc, esto nos sirve para poder adaptar la imagen del background restandole 17px de la barra de scroll en pc para evitar el scroll horizontal y quitandolo en movil para no cortar la imagen.
