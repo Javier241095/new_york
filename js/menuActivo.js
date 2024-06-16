0ead68f95c06f1e0f194bfaa9cfaaf0806c7b0a6
Javier241095 <137368130+Javier241095@users.noreply.github.com>
2024-06-16 14:41:57 -0300

    agregado de descripciones en el css y js

--- js/menuActivo.js
+++ js/menuActivo.js
@@ -40,18 +40,20 @@ SubMenuActivo(spanFlecha[1], 1, 0);
 function SubMenuActivo( spanFlecha = [''], flechaActiva = 0, flechaDesactiva = 1){
   for (let i = 0; i < SubMenus.length; i++) {
       SubMenus[i].addEventListener('click', (e)=>{
-        //Esta variable se utiliza para buscar el hermano siguiente del SPAN que seria el UL donde se encuentra la lista del SUBMENU
+        //Esta variable se utiliza para buscar el hermano siguiente del SPAN que seria el UL donde se encuentra la lista del SUBMENU.
         let listaSubMenu = spanFlecha.nextElementSibling;
   
         if(i == flechaActiva){
-          //esta clase se aplica para rotar la flecha hacia abajo para indicar que esta desplegado el SUBMENU
+          //esta clase se aplica para rotar la flecha hacia abajo para indicar que esta desplegado el SUBMENU.
           spanFlecha.classList.toggle('flecha_abajo')
-          //Esta variable guarda el alto obtenido del "scrollHeight" espacio necesario para mostrar el listado del SUBMENU
+          //Esta variable guarda el alto obtenido del "scrollHeight" espacio necesario para mostrar el listado del SUBMENU.
           let altoSubMenu = `${listaSubMenu.scrollHeight}px`;
-          //Esta variable se encarga de aplicar el valor del "height" para mostra el SUBMENU
+          //Esta propiedad se encarga de aplicar el valor del "height" para mostra el SUBMENU o volver el valor del "height" a 0px para ocultarlo.
           listaSubMenu.style.height = listaSubMenu.style.height == altoSubMenu ?  '0px': altoSubMenu;
+          //Esta propiedad se encarga de aplicar la CLASS "altoSubMenu" atravez de css. Linea del codigo 188.
           listaSubMenu.classList.toggle('altoSubMenu')
         } else if(i == flechaDesactiva ){
+          //Estas propiedades se utiliza para ocultar el SUBMENU que no este ACTIVO evitando tener los dos SUBMENU abiertos al mismo tiempo que generaria que el contenido de las listas sea mayor al alto del MENU HAMBURGUESA.
           listaSubMenu.style.height = '0px'
           listaSubMenu.classList.remove('altoSubMenu')
           spanFlecha.classList.remove('flecha_abajo')
